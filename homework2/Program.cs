167d0489201faa814a762845ca3d13ed7c2a4a56
Zhana Orkodashvili <72037472+zhana-orkodashvili@users.noreply.github.com>
2024-01-16 00:26:44 +0400

    week 4 homework

---
+++ homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Zhana Orkodashvili");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor= ConsoleColor.White;
+            Console.WriteLine("Enter your name:");
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
